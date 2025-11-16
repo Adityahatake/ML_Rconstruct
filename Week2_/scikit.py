9bc5f5d26d1b2bf73cfd0b84439c14051d6ed350
Adityahatake <shivanshdaksh567@gmail.com>
2025-11-16 22:49:15 +0530

    u

--- Week2_/scikit.py
+++ Week2_/scikit.py
@@ -14,6 +14,8 @@ from sklearn.externals import joblib
 # Comprehensive scikit-learn example
 import matplotlib.pyplot as plt
 
+
+
 # 1. Loading and preparing data
 print("1. Data Loading and Preparation")
 iris = load_iris()
