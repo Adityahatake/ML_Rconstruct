7376d65f8bcbcd259580e20ef91b222c0c575dfb
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-08 23:53:46 +0530

    I

--- Py_Basics/lambda.py
+++ Py_Basics/lambda.py
@@ -1,2 +1,3 @@
+# lambda = anonymous function
 square = lambda x: x*x
 print(square(5))
---
+++ Py_Basics/map.py
@@ -0,0 +1,4 @@
+# map = apply function to every element
+nums = [1,2,3]
+out = list(map(lambda x: x*2, nums))
+print(out)
