e3abe6c84ebad44cbd627fba0f5c74403d702498
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-06-19 23:51:05 +0530

    up

---
+++ Week1_Basics/numpy_excercise.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr=np.arange(1,31).reshape(5,6)
+slice=arr[:,3]
+l=[1,2,3,4,5]
+a1=np.array(l)
+a2=np.array([6,7,8,9,10])
+a3=l+a2
+
+d="hello"
+print(type(l))
+
+print(a3)
+print(arr.dtype)
